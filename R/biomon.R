72acd937065e1c1ec842ff0468c187cbd9962c54
Rodrigo Buhler <rodrigo.buhler@ymail.com>
2015-02-10 23:53:44 -0200

    Version 2.0
    First productive release.

---
+++ R/biomon.R
@@ -0,0 +1,12 @@
+#' Graphical User Interface for Biomon
+#' 
+#' @title Function to call Biomon's interface
+#'  
+#' @import shiny
+#' @import biomonCore
+#' @import analyz
+
+#' @export
+biomon <- function(){
+  shiny::runApp(system.file('shiny', package='biomonView'))
+}
